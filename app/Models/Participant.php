dc980db20ca12609684756fab38c14423fbeb96d
Njie <njie@wak-studio.com>
2026-01-05 11:32:41 -0400

    ajout de genesys

--- app/Models/Participant.php
+++ app/Models/Participant.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Base\Genesys;
 
 class Participant extends Model
 {
@@ -11,16 +12,35 @@ class Participant extends Model
         'zipcode', 'optin', 'bysms', 'byemail', 'slug', 'source'
     ];
 
+    // Relations
     public function films()
-{
-    return $this->belongsToMany(Film::class, 'participant_film');
-}
-
+    {
+        return $this->belongsToMany(Film::class, 'participant_film');
+    }
 
     public function dotations()
-{
-    return $this->belongsToMany(Dotation::class, 'participant_dotation');
-}
+    {
+        return $this->belongsToMany(Dotation::class, 'participant_dotation');
+    }
 
+    // --- Accessors pour déchiffrement ---
+    public function getFirstnameAttribute($value)
+    {
+        return $value ? Genesys::Decrypt($value) : '';
+    }
 
+    public function getLastnameAttribute($value)
+    {
+        return $value ? Genesys::Decrypt($value) : '';
+    }
+
+    public function getEmailAttribute($value)
+    {
+        return $value ? Genesys::Decrypt($value) : '';
+    }
+
+    public function getTelephoneAttribute($value)
+    {
+        return $value ? Genesys::Decrypt($value) : '';
+    }
 }
